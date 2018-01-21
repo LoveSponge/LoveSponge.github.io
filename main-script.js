243e019989f30a7feeccbacec78fb30ec948ec98
Guy K <guy.kellermann@gmail.com>
2018-01-21 14:07:29 -0500

    completed responsive design. completed descriptions for each project. completed the colour rotation. added colours. fixed a couple of bugs with the viewer when open but not hovering. TODO Rewrite JS for RxJS Add more projects Complete Scheduler Add links to CV, Github, Facebook, Twitter, Email.

--- main-script.js
+++ main-script.js
@@ -51,7 +51,8 @@ function peek(t) {
 }
 
 function offPeek(t) {
- if (t.children[3].children[2].innerHTML === "close") {
+ // console.log(t.querySelector('.card-actions').querySelector('.view').innerHTML);
+ if (t.querySelector('.card-actions').querySelector('.view').innerHTML === "close") {
   // do nothing if the viewer is currently open
  } else {
   if (window.innerWidth >= 768) {
@@ -62,4 +63,67 @@ function offPeek(t) {
    // do nothing for mobile view
   }
  }
+}
+
+let currentScheme = "white";
+function changeColour() {
+ function cycler(scheme) {
+  document.documentElement.style.setProperty('--header-colour', scheme.hc);
+  document.documentElement.style.setProperty('--primary-colour', scheme.pc);
+  document.documentElement.style.setProperty('--secondary-colour', scheme.sc);
+  document.documentElement.style.setProperty('--accent-colour', scheme.ac);
+  document.documentElement.style.setProperty('--header-colour-text', scheme.hct);
+  document.documentElement.style.setProperty('--primary-colour-text', scheme.pct);
+  document.documentElement.style.setProperty('--secondary-colour-text', scheme.sct);
+  document.documentElement.style.setProperty('--accent-colour-text', scheme.act);
+ }
+ const colourSchemes = {
+  white: {
+   hc: "#616161",
+   pc: "white",
+   sc: "white",
+   ac: "#00E676",
+   hct: "white",
+   pct: "black",
+   sct: "black",
+   act: "black"
+  },
+  blue: {
+   hc: "#1976D2",
+   pc: "#2196F3",
+   sc: "#64B5F6",
+   ac: "#FFC400",
+   hct: "white",
+   pct: "white",
+   sct: "white",
+   act: "black"
+  },
+  purple: {
+   hc: "#512DA8",
+   pc: "#3F51B5",
+   sc: "#9575CD",
+   ac: "#F50057",
+   hct: "white",
+   pct: "white",
+   sct: "black",
+   act: "black"
+  }
+ }
+ if (currentScheme === "white") {
+  cycler(colourSchemes.blue);
+  currentScheme = "blue";
+ } else if (currentScheme === "blue") {
+  cycler(colourSchemes.purple);
+  currentScheme = "purple";
+ } else if (currentScheme === "purple") {
+  cycler(colourSchemes.white);
+  currentScheme = "white";
+ }
+}
+
+let instructions = document.querySelector(".instructions");
+if (window.innerWidth >= 768) {
+ instructions.innerHTML = "Hovering over any of the elements to the right will allow you to peek, clicking view will open a larger viewer for you to browse the project which you would like to see"; // text for web width
+} else {
+ instructions.innerHTML = "Swipe down to see the projects I have added to my portfolio, tapping 'view' will enlarge the viewer so that you can see them in more detail."; // text for mobile width
 }
