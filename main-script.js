093fd9b0236c3c602052636b25d940467ce1ebdc
Guy K <guy.kellermann@gmail.com>
2018-02-06 17:57:07 -0500

    Added scripts to handle click events Adding responsive CSS

---
+++ main-script.js
@@ -0,0 +1,48 @@
+function onload() {
+ let iframes = document.querySelectorAll('iframe');
+
+ for (iframe of iframes) {
+  iframe.setAttribute('scrolling', 'no');
+ }
+}
+
+function foo(target) {
+ console.log();
+ let content = document.querySelector('.content');
+ let cards = document.querySelectorAll('.card');
+ let $thisIframe = target.parentNode.parentNode.querySelector('.card-iframe').querySelector('iframe');
+
+ if (target.innerText === 'View') {
+  for (let card of cards) {
+   if (card === target.parentNode.parentNode) {
+    content.setAttribute('style', 'grid-template-rows: 1fr; grid-template-columns: 1fr;');
+    target.innerText = 'Close';
+    document.body.setAttribute('style', 'overflow-y: hidden;');
+    $thisIframe.setAttribute('scrolling', 'yes');
+   } else {
+    card.setAttribute('style', 'display: none;');
+   }
+
+  }
+ } else if (target.innerText === 'Close') {
+  target.innerText = 'View';
+  content.setAttribute('style', '');
+  document.body.setAttribute('style', '');
+  $thisIframe.setAttribute('scrolling', 'no');
+  for (let card of cards) {
+   card.setAttribute('style', '');
+  }
+ } else if (target.innerText === 'Blog') {
+  // redirect to blog page
+  alert('Coming soon!');
+  // console.log(target.innerText);
+ }
+
+}
+
+
+function iterateOverTargets(targets) {
+ for (let target of targets) {
+  // do something
+ }
+}
